ddc1b8900e5a0c63e8a95cc415e1347b4e4a4a7c
Alex Chavez <alex.chavez@machinima.com>
2018-06-25 19:08:41 -0700

    Update Facebook API version to v3.0

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 module.exports = FacebookInsightStream;
 
-var util = require( 'util' );
-var sugar = require( 'sugar' );
-var stream = require( 'stream' );
-var extend = require( 'extend' );
-var request = require( 'request' );
-var Promise = require( 'bluebird' );
+var util = require('util');
+var sugar = require('sugar');
+var stream = require('stream');
+var extend = require('extend');
+var request = require('request');
+var Promise = require('bluebird');
 
-request = Promise.promisifyAll( request )
+request = Promise.promisifyAll(request)
 
-var BASEURL = 'https://graph.facebook.com/v2.12';
+var BASEURL = 'https://graph.facebook.com/v3.0';
 // Missing data is flagged by the error code 100
 // GraphMethodException error:
 // Object with ID 'some_id' does not exist,
@@ -25,70 +25,70 @@ var EDGEMAP = {
     post: 'insights'
 }
 
-util.inherits( FacebookInsightStream, stream.Readable )
+util.inherits(FacebookInsightStream, stream.Readable)
 /** this module is a facebook-insights read stream built over node readable
  * stream. It provide stream api to read insights data from facebook accounts,
  * currently supporting only pages-insight, posts-insights and app-insights.
 */
-function FacebookInsightStream( options ) {
-    stream.Readable.call( this, { objectMode: true } );
+function FacebookInsightStream(options) {
+    stream.Readable.call(this, { objectMode: true });
     var listItems = options.itemList;
     var isFunction = typeof listItems === 'function';
-    if ( !isFunction ) {
+    if (!isFunction) {
         listItems = () => options.itemList
     }
 
     options.listItems = listItems
-    options.edge = EDGEMAP[ options.node ];
+    options.edge = EDGEMAP[options.node];
     this.options = options;
 }
 
 // _read will be called once for each collected item
-FacebookInsightStream.prototype._read = function ( ) {
+FacebookInsightStream.prototype._read = function () {
 
-    if ( ! this.items ) {
-        return this._init( this._read.bind( this ) )
-            .catch( this.emit.bind( this, 'error') )
+    if (!this.items) {
+        return this._init(this._read.bind(this))
+            .catch(this.emit.bind(this, 'error'))
     }
-    if ( ! this.items.length ) {
-        return this.push( null )
+    if (!this.items.length) {
+        return this.push(null)
     }
     var metrics = this.options.metrics.clone();
-    var item = this.items[ this.items.length - 1 ];
+    var item = this.items[this.items.length - 1];
     var events = this.events.clone();
 
-    this._collect( metrics, item, {}, events )
-        .tap( this.removeItem.bind( this ) )
-        .then( this._handleData.bind( this ) )
+    this._collect(metrics, item, {}, events)
+        .tap(this.removeItem.bind(this))
+        .then(this._handleData.bind(this))
 }
 
 FacebookInsightStream.prototype.removeItem = function () {
-    var idx = this.items.indexOf( this.item );
-    this.items.splice( idx, 1 );
+    var idx = this.items.indexOf(this.item);
+    this.items.splice(idx, 1);
 }
 
-FacebookInsightStream.prototype._handleData = function ( data ) {
-    return this.push( data )
+FacebookInsightStream.prototype._handleData = function (data) {
+    return this.push(data)
 }
 
-FacebookInsightStream.prototype._init = function ( callback ) {
+FacebookInsightStream.prototype._init = function (callback) {
     var options = this.options;
 
     // building url pattern for all the request
     var until = Date.now();
     var since = new Date();
-    since = since.setDate( since.getDate() - options.pastdays )
+    since = since.setDate(since.getDate() - options.pastdays)
 
     // fb ask for timestamp in seconds
-    until = Math.round( until / 1000 );
-    since = Math.round( since / 1000 );
+    until = Math.round(until / 1000);
+    since = Math.round(since / 1000);
 
     var path = [
         BASEURL,
         '{id}',
         options.edge,
         '{metric}',
-    ].join( '/' )
+    ].join('/')
 
     var hasEvents = options.events && options.events.length;
     var breakdowns = options.breakdowns;
@@ -106,8 +106,8 @@ FacebookInsightStream.prototype._init = function ( callback ) {
         return `${key}=${queryObj[key]}`
     }).join('&')
 
-    if ( breakdowns && breakdowns.length ) {
-        for ( var i = 0; i < breakdowns.length; i += 1 ) {
+    if (breakdowns && breakdowns.length) {
+        for (var i = 0; i < breakdowns.length; i += 1) {
             query += `&breakdowns[${i}]=${breakdowns[i]}`
         }
     }
@@ -115,32 +115,32 @@ FacebookInsightStream.prototype._init = function ( callback ) {
     // this url is urlPattern shared by all the requests
     // each request using thie pattern should replace the
     // {id} and {metric} place holders with real values
-    this.url = [ path, query ].join( '?' )
+    this.url = [path, query].join('?')
 
     // options.itemlist is a function that can return either array of items or
     // or a promise that resolved with array of items
     var itemList = options.listItems();
-    return Promise.resolve( itemList )
-        .bind( this )
-        .map( this._initItem, { concurrency: 3 } )
+    return Promise.resolve(itemList)
+        .bind(this)
+        .map(this._initItem, { concurrency: 3 })
         // Calling _initItem on each object might have resulted in some
         // skipped objects. These will still have been returned in the mapped
         // array as `undefined` elements. This filter removes them.
         .filter(Boolean)
-        .then( function ( items ) {
+        .then(function (items) {
             this.items = items;
             this.events = options.events || [];
             this.total = items.length;
             this.loaded = 0;
             return callback();
         })
-        .catch( function ( error ) {
-            var retry = this._init.bind( this, callback );
-            return this.handleError( error, retry )
+        .catch(function (error) {
+            var retry = this._init.bind(this, callback);
+            return this.handleError(error, retry)
         })
 }
 
-FacebookInsightStream.prototype._initItem = function ( item ) {
+FacebookInsightStream.prototype._initItem = function (item) {
     var id = typeof item === 'string' ? item : item.id
     var options = this.options;
     var model = {
@@ -152,30 +152,30 @@ FacebookInsightStream.prototype._initItem = function ( item ) {
     var url = `${model.base}/${model.id}?access_token=${model.token}`
 
     var title = 'FACEBOOK ' + options.node.toUpperCase();
-    console.log( new Date().toISOString(), title, url )
+    console.log(new Date().toISOString(), title, url)
 
     return FacebookInsightStream.apiCall(url)
-        .bind( this )
-        .get( 1 )
-        .then( JSON.parse )
-        .then( errorHandler.bind( null, options ) )
-        .then( function ( data ) {
+        .bind(this)
+        .get(1)
+        .then(JSON.parse)
+        .then(errorHandler.bind(null, options))
+        .then(function (data) {
             var result = {
                 id: id,
                 name: data.name || data.message || data.story,
                 token: item.token || options.token
             }
-            if ( options.node === 'post' ) {
+            if (options.node === 'post') {
                 result.createdTime = data.created_time
             }
             return result
         })
-        .catch( SkippedError, function ( error ) {
-            console.warn( 'facebook-insights skipped error', error );
+        .catch(SkippedError, function (error) {
+            console.warn('facebook-insights skipped error', error);
         })
-        .catch( function ( error ) {
-            var retry = this._initItem.bind( this, item );
-            return this.handleError( error, retry )
+        .catch(function (error) {
+            var retry = this._initItem.bind(this, item);
+            return this.handleError(error, retry)
         })
 }
 
@@ -187,27 +187,27 @@ FacebookInsightStream.prototype._initItem = function ( item ) {
  * of the current metric to the appropriate key in the buffer.
  * finally we generating single row for each day.
  */
-FacebookInsightStream.prototype._collect = function ( metrics, item, buffer, events ) {
+FacebookInsightStream.prototype._collect = function (metrics, item, buffer, events) {
     var options = this.options;
     var hasEvents = events && events.length;
     // done with the current item
-    if ( ! metrics.length && ! hasEvents ) {
-        var data = Object.keys( buffer ).map( function ( key ) {
-            var row = buffer[ key ];
+    if (!metrics.length && !hasEvents) {
+        var data = Object.keys(buffer).map(function (key) {
+            var row = buffer[key];
 
             // if the key is constructed with numerous attributes,
             // take the datetime information
-            row.date = key.split( '__' )[ 0 ];
-            row[ options.node + 'Id' ] = item.id;
-            row[ options.node + 'Name' ] = item.name;
+            row.date = key.split('__')[0];
+            row[options.node + 'Id'] = item.id;
+            row[options.node + 'Name'] = item.name;
             // set created_time for posts
-            if ( options.node === 'post' ) {
-                row[ 'created_time' ] = item.createdTime;
+            if (options.node === 'post') {
+                row['created_time'] = item.createdTime;
             }
             return row;
         })
 
-        this.emit( 'progress', {
+        this.emit('progress', {
             total: this.total,
             loaded: ++this.loaded,
             message: '{{remaining}} ' + options.node + 's remaining'
@@ -217,96 +217,96 @@ FacebookInsightStream.prototype._collect = function ( metrics, item, buffer, eve
 
     // for the audience API, we just use one metric ['app_event']
     // with a few events
-    var _metric = metrics[ metrics.length -1 ] || options.metrics[ 0 ];
+    var _metric = metrics[metrics.length - 1] || options.metrics[0];
     var model = { id: item.id, metric: _metric }
 
     var _ev;
     var _agg;
-    if ( hasEvents ) {
+    if (hasEvents) {
         // extend the query model with event name
         // and aggregation type
-        _ev = events[ events.length - 1 ];
-        _agg = aggregationType( _ev );
+        _ev = events[events.length - 1];
+        _agg = aggregationType(_ev);
 
-        extend( model, { ev: _ev, agg: _agg } );
+        extend(model, { ev: _ev, agg: _agg });
     }
 
-    var url = strReplace( this.url, model );
+    var url = strReplace(this.url, model);
     url = url.replace(/access_token=.*?&/, `access_token=${item.token}&`)
     var title = 'FACEBOOK ' + options.node.toUpperCase();
 
-    console.log( new Date().toISOString(), title, url );
+    console.log(new Date().toISOString(), title, url);
 
     return FacebookInsightStream.apiCall(url)
-        .get( 1 )
-        .then( JSON.parse )
-        .then( errorHandler.bind( null, options ) )
-        .get( 'data' )
-        .bind( this )
-        .then( function ( data ) {
+        .get(1)
+        .then(JSON.parse)
+        .then(errorHandler.bind(null, options))
+        .get('data')
+        .bind(this)
+        .then(function (data) {
             // in case that there is no data for a given metric
             // we will skip to the next metric
-            if ( ! data.length ) {
+            if (!data.length) {
                 var error = new Error('No data found for metric ' + _metric);
                 error.skip = true;
                 throw error;
             }
             // in app insight the returned data is list of values
             // in page insight its object that include the list of values
-            return data[ 0 ].values || data
+            return data[0].values || data
         })
-        .each( function ( val ) {
+        .each(function (val) {
             var key = val.end_time || val.time || 'lifetime';
             // when using breakdowns we get numerous results for
             // the same date therefore we need to identify unique
             // keys for the buffer by the date and different breakdowns
             // we're using the '__' to later seperate the date
-            Object.keys( val.breakdowns || {} ).forEach( function ( b ) {
+            Object.keys(val.breakdowns || {}).forEach(function (b) {
                 key += `__${val.breakdowns[b]}`
             });
 
-            buffer[ key ] || ( buffer[ key ] = {} )
+            buffer[key] || (buffer[key] = {})
             // either a metric or an event
             var column = _ev ? _ev : _metric;
-            if ( typeof val.value === 'object' ) {
-                Object.keys( val.value ).map( function ( subMetric ) {
+            if (typeof val.value === 'object') {
+                Object.keys(val.value).map(function (subMetric) {
                     var col = column + '_' + subMetric;
-                    buffer[ key ][ col ] = val.value[subMetric];
+                    buffer[key][col] = val.value[subMetric];
                 })
             } else {
-                buffer[ key ][ column ] = val.value || null;
+                buffer[key][column] = val.value || null;
             }
 
             // set breakdowns data if given
             var breakdowns = options.breakdowns;
-            if ( !breakdowns || !val.breakdowns ) {
+            if (!breakdowns || !val.breakdowns) {
                 return;
             }
 
-            for ( var i = 0; i < breakdowns.length; i += 1 ) {
+            for (var i = 0; i < breakdowns.length; i += 1) {
                 // options breakdown
-                var b = breakdowns[ i ];
+                var b = breakdowns[i];
 
-                if ( val.breakdowns[ b ] ) {
-                    buffer[ key ][ b ] = val.breakdowns[ b ];
+                if (val.breakdowns[b]) {
+                    buffer[key][b] = val.breakdowns[b];
                 }
             }
         })
-        .catch( SkippedError, function ( error ) {
-            console.warn( 'facebook-insights skipped error', error );
+        .catch(SkippedError, function (error) {
+            console.warn('facebook-insights skipped error', error);
         })
-        .then( function () {
+        .then(function () {
             // remove the current paramater when done
-            var _metricIdx = metrics.indexOf( _metric );
-            var _evIdx = events.indexOf( _ev );
-            metrics.splice( _metricIdx, 1 );
-            events.splice( _evIdx, 1 );
+            var _metricIdx = metrics.indexOf(_metric);
+            var _evIdx = events.indexOf(_ev);
+            metrics.splice(_metricIdx, 1);
+            events.splice(_evIdx, 1);
 
-            return this._collect( metrics, item, buffer, events );
+            return this._collect(metrics, item, buffer, events);
         })
-        .catch( function ( error ) {
+        .catch(function (error) {
             var retry = this._collect.bind(this, metrics, item, buffer, events);
-            return this.handleError( error, retry );
+            return this.handleError(error, retry);
         })
 }
 
@@ -321,11 +321,11 @@ FacebookInsightStream.prototype._collect = function ( metrics, item, buffer, eve
  * @param  {Error}  error
  * @param  {Function} retry - the function that should be invoked on retry
  */
-FacebookInsightStream.prototype.handleError = function ( error, retry ) {
-    if ( error.retry === true ) {
+FacebookInsightStream.prototype.handleError = function (error, retry) {
+    if (error.retry === true) {
         return retry();
     } else {
-        this.emit( 'error', error );
+        this.emit('error', error);
     }
 }
 
@@ -337,19 +337,19 @@ FacebookInsightStream.prototype.handleError = function ( error, retry ) {
  * @returns  {Promise}
  */
 FacebookInsightStream.apiCall = function (url) {
-    return request.getAsync( url )
+    return request.getAsync(url)
 }
 
 /**
 */
-function SkippedError ( error ) {
+function SkippedError(error) {
     return error.skip === true;
 }
 
 /**
 */
-function errorHandler ( options, body ) {
-    if ( body.error ) {
+function errorHandler(options, body) {
+    if (body.error) {
         var missingItem = body.error.code === MISSING_ERROR_CODE
         body.error.skip = (options.ignoreMissing && missingItem)
             || body.error.code === NOT_SUPPORTED_CODE
@@ -362,9 +362,9 @@ function errorHandler ( options, body ) {
 
 /**
 */
-function strReplace ( string, model ) {
-    Object.keys( model ).each( function ( name ) {
-        string = string.replace( '{' + name + '}', model[ name ] );
+function strReplace(string, model) {
+    Object.keys(model).each(function (name) {
+        string = string.replace('{' + name + '}', model[name]);
     })
 
     return string;
@@ -372,11 +372,11 @@ function strReplace ( string, model ) {
 
 /**
 */
-function aggregationType ( ev ) {
-    var events = [ 'fb_ad_network_imp', 'fb_ad_network_click' ];
+function aggregationType(ev) {
+    var events = ['fb_ad_network_imp', 'fb_ad_network_click'];
 
-    var shouldUseCount = ev && events.indexOf( ev ) > -1;
-    if ( shouldUseCount ) {
+    var shouldUseCount = ev && events.indexOf(ev) > -1;
+    if (shouldUseCount) {
         return 'COUNT'
     }
     return 'SUM';
